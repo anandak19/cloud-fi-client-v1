d64c866c2532269b7c1ebb15bf5b60a075f6ad41
Anandakrishnan H <anandak19h@gmail.com>
2026-05-18 00:03:57 +0530

    fix: fix ngrok error

--- src/app/core/interceptors/http/http-interceptor.ts
+++ src/app/core/interceptors/http/http-interceptor.ts
@@ -2,11 +2,15 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'environments/environment.development';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-    const apiUrl = environment.apiUrl;
-  console.log(apiUrl);
+  const apiUrl = environment.apiUrl;
   const newReq = req.clone({
+    // remove this property befrore production build
+    setHeaders: {
+      'ngrok-skip-browser-warning': 'true',
+    },
+
     url: `${apiUrl}/api/${req.url}`,
-    withCredentials: true
+    withCredentials: true,
   });
   return next(newReq);
 };
